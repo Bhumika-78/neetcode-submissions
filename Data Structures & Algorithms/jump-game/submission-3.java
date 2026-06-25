7f7d66689ac7ba5abb475886989f90e851bfa09d
Bhumika <bhumika.codes@gmail.com>
2026-06-25 23:24:18 +0530

    Add: jump-game - submission-3

---
+++ Data Structures & Algorithms/jump-game/submission-3.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int good = n - 1;
+        for(int i = n - 2 ; i >= 0; i--){
+            if(i + nums[i] >= good) good = i;
+        }
+        return good == 0;
+    }
+}
